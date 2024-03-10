f7c9a6799e4d0f96a2f093b257ea96f144b40501
Maikon Cordeiro <maikon.lancamento@gmail.com>
2024-03-10 17:05:31 -0300

    tarefa executada conforme descrição do projeto. Aplicado Array , funções , e métodos.

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(frase) {
+    let frasesPoli = frase.toLowerCase().replace(/\s/g, '');
+    let fraseVerify = "";
+
+    for (let i = frasesPoli.length - 1; i >= 0; i--) {
+        fraseVerify += frasesPoli[i];
+    } return frasesPoli === fraseVerify;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+console.log(isPalindrome('Radar'));
+console.log(isPalindrome('A sacada da casa'));
+console.log(isPalindrome('A torre da derrota'));
+console.log(isPalindrome('Programação'))
+
+// O nome da função deve ser arrayMaxMin;
+// A função deverá receber o array por parâmetro;
+// Sua função deve obrigatoriamente utilizar laço de repetição para percorrer e analisar o array recebido por parâmetro;
+// Sua função deve retornar um novo array onde no índice 0 será apresentado o valor mínimo e no índice 1 o valor máximo.
+
+function arrayMaxMin(arr) {
+
+    if (arr.length === 0) {
+        return "O array está vazio.";
+    }
+
+    let minimo = arr[0];
+    let maximo = arr[0];
+
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minimo) {
+            minimo = arr[i];
+        }
+        if (arr[i] > maximo) {
+            maximo = arr[i];
+        }
+    }
+
+
+    return [minimo, maximo];
 }
+
+console.log(arrayMaxMin([3, 1, 9, 7, 4, 2]));  
+console.log(arrayMaxMin([-5, 0, 15, 8, -2]));  
+console.log(arrayMaxMin([])); 
+
